df555740687e5e72f62d7834b35725ccb76fdc74
prarthna9876-commits <prarthna9876@gmail.com>
2026-07-28 13:58:05 +0530

    Add files via upload

---
+++ WorkoutSession.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WorkoutSession = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSIONS</h1>
+        <p>Our workout sessions are designed to help you achieve your fitness goals.
+          Whether you're a beginner or an experienced athlete,
+          we have a session for you.</p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+    </section>
+  )
+}
+
+export default WorkoutSession;
